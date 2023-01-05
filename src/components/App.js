d6d255e895414ea71f26c486fc987234ef0ff815
Aman Arya <117149824+aman7714@users.noreply.github.com>
2023-01-05 19:15:34 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,20 @@
 import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
+
 function App() {
+      const show = () =>{
+           document.getElementById("para").style.visibility = "visible";
+      }
+
   return (
     <div id="main">
       // Do not alter the main div
+      <>
+        <button onClick={show} id="click"></button>
+        <p style={{visibility:"hidden"}} id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes 
+             me so happy</p>
+      </>
     </div>
   );
 }
